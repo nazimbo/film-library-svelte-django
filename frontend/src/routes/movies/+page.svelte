7f06fd2f42541f234f5daaa343242ace7491ff8e
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-03-19 15:02:09 +0100

    Add functionality to add a movie to the movie list

--- frontend/src/routes/movies/+page.svelte
+++ frontend/src/routes/movies/+page.svelte
@@ -1,12 +1,26 @@
 <script>
   import { MovieStore } from "../../movie-store";
+
+  let handleClick = () =>
+    MovieStore.update((movies) => {
+      let newMovie = {
+        id: 4,
+        title: "The Room",
+        year: 2003,
+        director: "Tommy Wiseau",
+        cast: ["Tommy Wiseau", "Juliette Danielle", "Greg Sestero"],
+      };
+      return [...movies, newMovie];
+    });
 </script>
 
 <h1>Movies</h1>
 <ul>
   {#each $MovieStore as movie}
     <li>
-      <a href="movies/{movie.id}">{movie.title} </a>- {movie.year}
+      <a href="movies/{movie.id}">{movie.title} </a>
     </li>
   {/each}
 </ul>
+
+<button on:click={handleClick}>Add a movie</button>
---
+++ frontend/src/routes/movies/add/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { MovieStore } from "../../../movie-store";
+
+  let title = "";
+  let director = "";
+
+  let handleSubmit = () => {
+    MovieStore.update((movies) => {
+      let id = movies[movies.length - 1].id + 1;
+      let newMovie = {
+        id: id,
+        title: title,
+        director: director,
+      };
+      return [...movies, newMovie];
+    });
+    goto("/movies");
+  };
+</script>
+
+<h1>Add a movie</h1>
+<form on:submit|preventDefault={handleSubmit}>
+  <div>
+    <input type="text" placeholder="title" bind:value={title} />
+  </div>
+
+  <div>
+    <input type="text" placeholder="director" bind:value={director} />
+  </div>
+
+  <button class="submit">Add</button>
+</form>
